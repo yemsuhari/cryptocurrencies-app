8ee0ab1683b4ef79712edd9a65f70f7c4460b66a
yemsuhari <albertpetrovich3@gmail.com>
2022-06-16 15:27:56 +0300

    Initial Commit

---
+++ cryptocurrencies-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cryptocurrencies-app
+//
+//  Created by Fyodor Vladimirov on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
