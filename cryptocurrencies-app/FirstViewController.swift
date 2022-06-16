e37c4628dbc59845c4f07d05f597e2f1ac6e69a0
yemsuhari <albertpetrovich3@gmail.com>
2022-06-16 15:37:30 +0300

    - setting up

---
+++ cryptocurrencies-app/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  cryptocurrencies-app
+//
+//  Created by Fyodor Vladimirov on 16.06.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- cryptocurrencies-app/SceneDelegate.swift
+++ cryptocurrencies-app/SceneDelegate.swift
@@ -16,7 +16,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let baseWindowScene = (scene as? UIWindowScene) else { return }
+        
+        let startingViewController = FirstViewController()
+        
+        let navigationController = UINavigationController()
+        
+        navigationController.pushViewController(startingViewController, animated: true)
+        
+        let window = UIWindow(windowScene: baseWindowScene)
+        
+        window.makeKeyAndVisible()
+        
+        window.rootViewController = navigationController
+        
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
